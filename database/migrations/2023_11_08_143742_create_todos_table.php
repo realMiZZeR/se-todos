f28dba7e432e49050fcbe57e81dfcaa711ee1898
realMiZZeR <maxcan2work@gmail.com>
2023-11-08 21:48:23 +0700

    Created «user_roles» and «todos» migrations + models. Added column «role_id» in users migration.

--- database/migrations/2023_11_08_143742_create_todos_table.php
+++ database/migrations/2023_11_08_143742_create_todos_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('user_roles');
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('todos');
     }
 };
